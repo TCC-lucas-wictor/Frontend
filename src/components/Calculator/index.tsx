eae3493dccf1968cedef0780d805666a57b7b492
Wictorgirardi <wictor.girardi@gmail.com>
2023-07-20 23:26:07 -0300

    fix: cost function

--- src/components/Calculator/index.tsx
+++ src/components/Calculator/index.tsx
@@ -6,6 +6,7 @@ import * as Yup from 'yup'
 import MenuItem from '@mui/material/MenuItem'
 import Select from '@mui/material/Select'
 import { DefaultApi } from '~services/api'
+import { parseInt } from 'lodash'
 
 const RootStyle = styled('div')({
   background: '#EEF1FF',
@@ -37,11 +38,21 @@ const Calculator = () => {
   const [result, setResult] = useState(false)
   const [complexidade, setComplexidade] = useState('')
   const [inputs, setInputs] = useState('')
+  const [memory, setMemory] = useState('')
+  const [number, setNumber] = useState('')
   const [response, setResponse] = useState('')
+  const [type, setType] = useState('')
+  const [tCost, setTcost] = useState(0)
 
   const CalcSchema = Yup.object().shape({
     qtd_inputs: Yup.string().required('A quantidade de inputs é obrigatória'),
   })
+
+  const calculateLambdaCost = (requestsPerMonth, memoryMB, executionTimeMS) => {
+    const memoryGB = memoryMB / 1024
+    const cost = requestsPerMonth * memoryGB * executionTimeMS * 0.0000166667
+    return cost
+  }
 
   const formik = useFormik({
     initialValues: {
@@ -53,17 +64,21 @@ const Calculator = () => {
 
     onSubmit: () => {
       if (formik.values.complexidade === '1') {
-        setComplexidade('Linear(O(n))')
+        setType('predictDecisionTree')
+        setComplexidade('Constante')
       } else if (formik.values.complexidade === '2') {
-        setComplexidade('Linearítmica(O(n log n))')
+        setType('predictDecisionTree')
+        setComplexidade('Linear(O(n))')
       } else if (formik.values.complexidade === '3') {
-        setComplexidade('Quadrática(O(n²))')
+        setType('predictDecisionTree')
+        setComplexidade('Linearítmica(O(n log n))')
       } else if (formik.values.complexidade === '4') {
-        setComplexidade('Cúbica(O(n³)')
+        setType('predictPolynomialRegression')
+        setComplexidade('Quadrática(O(n²))')
       }
 
       setInputs(formik.values.qtd_inputs)
-      DefaultApi.post('predictPolynomialRegression', formik.values)
+      DefaultApi.post(type, formik.values)
         .then((res: Data) => {
           setResponse(res?.data[0])
           setResult(true)
@@ -71,8 +86,9 @@ const Calculator = () => {
         .catch((err) => {
           console.log(err)
         })
-
-      console.log(formik.values)
+      const total = calculateLambdaCost(number, memory, response)
+      console.log(total)
+      setTcost(total)
     },
   })
 
@@ -94,15 +110,31 @@ const Calculator = () => {
                   <>
                     <Box display={'flex'} flexDirection={'column'} gap={1} mx={6}>
                       <Typography variant="h5">Resultado:</Typography>
+                      {response > '900' ? (
+                        <Typography variant="subtitle1" mb={3}>
+                          Esta função excede o limite de tempo de execução de funções dentro do AWS Lambda, tente
+                          novamente.
+                        </Typography>
+                      ) : (
+                        <>
+                          <Typography variant="subtitle1" mb={3}>
+                            Para uma função de complexidade <b>{complexidade}</b> e quantidade de inputs <b>{inputs}</b>
+                            , Estimamos que o tempo de execução da sua função será de:
+                          </Typography>
 
-                      <Typography variant="subtitle1" mb={3}>
-                        Para uma função de complexidade <b>{complexidade}</b> e quantidade de inputs <b>{inputs}</b>,
-                        Estimamos que o tempo de execução da sua função será de:
-                      </Typography>
+                          <Typography variant="h5" mb={3} textAlign={'center'}>
+                            {parseFloat(response).toFixed(2)} segundos
+                          </Typography>
 
-                      <Typography variant="h5" mb={3} textAlign={'center'}>
-                        {response} segundos
-                      </Typography>
+                          <Typography variant="subtitle1" mb={3}>
+                            Estimamos que o custo de execução da sua requisição será de:
+                          </Typography>
+                          <Typography variant="h5" mb={3} textAlign={'center'}>
+                            $ {tCost.toFixed(2)}  USD/Mês
+                           
+                          </Typography>
+                        </>
+                      )}
 
                       <Button
                         variant="outlined"
@@ -131,10 +163,10 @@ const Calculator = () => {
                         <MenuItem disabled value="">
                           <em>Complexidade</em>
                         </MenuItem>
-                        <MenuItem value={'1'}>Linear(O(n))</MenuItem>
-                        <MenuItem value={'2'}>Linearítmica(O(n log n))</MenuItem>
-                        <MenuItem value={'3'}>Quadrática(O(n²))</MenuItem>
-                        <MenuItem value={'4'}>Cúbica(O(n³))</MenuItem>
+                        <MenuItem value={'1'}>Constante</MenuItem>
+                        <MenuItem value={'2'}>Linear(O(n))</MenuItem>
+                        <MenuItem value={'3'}>Linearítmica(O(n log n))</MenuItem>
+                        <MenuItem value={'4'}>Quadrática(O(n²))</MenuItem>
                       </Select>
                       <TextField
                         sx={{ height: '50px' }}
@@ -145,6 +177,49 @@ const Calculator = () => {
                         error={Boolean(touched.qtd_inputs && errors.qtd_inputs)}
                         helperText={touched.qtd_inputs && errors.qtd_inputs}
                       />
+                    </Box>
+                    <Box display={'flex'} flexDirection={'row'} gap={3}>
+                      <TextField
+                        sx={{ height: '50px' }}
+                        fullWidth
+                        type="number"
+                        label="Quantidade de Execuções da função"
+                        value={number}
+                        onChange={(e) => {
+                          setNumber(e.target.value)
+                        }}
+                        error={Boolean(touched.qtd_inputs && errors.qtd_inputs)}
+                        helperText={touched.qtd_inputs && errors.qtd_inputs}
+                      />
+                      <Select
+                        fullWidth
+                        sx={{ height: '56px' }}
+                        label="Memory"
+                        displayEmpty
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={memory}
+                        onChange={(e) => {
+                          setMemory(e.target.value)
+                        }}
+                      >
+                        <MenuItem disabled value="">
+                          <em>Mémoria Alocada</em>
+                        </MenuItem>
+                        <MenuItem value={128}>128 MB</MenuItem>
+                        <MenuItem value={512}>512 MB</MenuItem>
+                        <MenuItem value={1024}>1024 MB</MenuItem>
+                        <MenuItem value={1536}>1536 MB</MenuItem>
+                        <MenuItem value={2048}>2048 MB</MenuItem>
+                        <MenuItem value={3072}>3072 MB</MenuItem>
+                        <MenuItem value={4096}>4096 MB</MenuItem>
+                        <MenuItem value={5120}>5120 MB</MenuItem>
+                        <MenuItem value={6144}>6144 MB</MenuItem>
+                        <MenuItem value={7168}>7168 MB</MenuItem>
+                        <MenuItem value={8192}>8192 MB</MenuItem>
+                        <MenuItem value={9216}>9216 MB</MenuItem>
+                        <MenuItem value={10240}>10.240 MB</MenuItem>
+                      </Select>
                     </Box>
                     <Button
                       variant="outlined"
